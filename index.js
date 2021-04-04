fe4c4ddea481a9b3c26374cc4be54b3034014f14
melissa-tan <melissatan9683@gmail.com>
2021-04-03 19:49:16 -0700

    added instructions and node_modules in gitignore

---
+++ index.js
@@ -0,0 +1,9 @@
+// Title
+// Description
+// Table of Contents (contains corresponding sections)
+// Installation (installation instructions)
+// Usage (usage information)
+// License (badge added near the top of the readme,explains which license the application is covered under)
+// Contribution (contribution guidelines)
+// Tests (test instructions)
+// Questions (get github username, email address-how to reach me with additional questions)
